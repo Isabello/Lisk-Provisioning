c63b16ae4d139dcf4cbf5817d0800737f13b093a
Isabello <dell.isabella@gmail.com>
2016-05-11 17:54:16 -0500

    Update installLisk.sh
    added time service checks

--- installLisk.sh
+++ installLisk.sh
@@ -19,6 +19,49 @@ if [ "$USER" == "root" ]; then
   exit 1
 fi
 
+#Install NTP or Chrony for Time Management - Physical Machines only
+ if [[ -f "/etc/debian_version" &&  ! -f "/proc/user_beancounters" ]]; then
+   if pgrep -x "ntpd" > /dev/null
+   then
+      echo "√ ntp is running"
+   else
+      echo "X ntp is not running"
+      read -r -n 1 -p "Would like to install ntp? (y/n): " $REPLY
+      if [[  $REPLY =~ ^[Yy]$ ]]
+      then
+        sudo apt-get update
+        sudo apt-get install ntp -yyq
+        sudo service ntp stop
+        sudo ntpdate pool.ntp.org
+        sudo service ntp start
+      fi
+   fi
+ elif [[ -f "/etc/redhat-release" &&  ! -f "/proc/user_beancounters" ]]; then
+   if pgrep -x "ntpd" > /dev/null
+   then
+      echo "√ ntp is running"
+   else
+      if pgrep -x "chronyd" > /dev/null
+      then
+        echo "√ chrony is running"
+      else
+        echo "X ntp and chrony are not running"
+        read -r -n 1 -p "Would like to install ntp? (y/n): " $REPLY
+        if [[  $REPLY =~ ^[Yy]$ ]]
+        then
+      yum install ntp ntpdate ntp-doc
+      chkconfig ntpd on
+      ntpdate pool.ntp.org
+      /etc/init.d/ntpd start
+        fi
+      fi
+   fi
+ elif [[ -f "/proc/user_beancounters" ]]; then
+   echo "_ Running OpenVZ VM"
+ fi
+
+
+
 liskVersion=`curl -s https://downloads.lisk.io/lisk/test/ | grep $UNAME | cut -d'"' -f2`
 wget https://downloads.lisk.io/lisk/test/$liskVersion
 
@@ -39,4 +82,5 @@ cp ~/test/liskMemTuner.sh $defaultLiskLocation/$liskDir
 bash $defaultLiskLocation/$liskDir/liskMemTuner.sh
 
 bash lisk.sh start
+
 
